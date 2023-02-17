d379b1579b880cb3a999f99c456d13c75694356a
Rick Daalhuizen <rick.daalhuizen@student.kdg.be>
2023-02-18 00:23:50 +0100

    first commit

--- README.md
+++ README.md
@@ -7,3 +7,29 @@ This part implements basics of the following concepts:
 2. Commit.
 3. Commit chaining.
 4. Branch.
+
+Run:
+```
+npm run build && npm run test
+```
+
+Output:
+```bash
+Branch { commits: [], name: 'main' }
+Create commit: (0) My first commit!
+Create commit: (1) My second commit!
+Create commit: (2) My third commit!
+[
+  Commit { id: 2, parentId: 1, message: 'My third commit!' },
+  Commit { id: 1, parentId: 0, message: 'My second commit!' },
+  Commit { id: 0, parentId: null, message: 'My first commit!' }
+]
+Checkout to: develop
+Create commit: (3) My commit in develop!
+[
+  Commit { id: 3, parentId: 2, message: 'My commit in develop!' },
+  Commit { id: 2, parentId: 1, message: 'My third commit!' },
+  Commit { id: 1, parentId: 0, message: 'My second commit!' },
+  Commit { id: 0, parentId: null, message: 'My first commit!' }
+]
+```
--- git.ts
+++ git.ts
@@ -1,48 +1,92 @@
 interface Git {
-  repository: string
-  lastCommitId: number;
-  branches: Branch[],
-  head: Branch;
-}
-
-class Git {
   repository: string;
-  lastCommitId: number;
   branches: Branch[];
-  head: Branch;
-
-  constructor(repository: string) {
-    this.lastCommitId = -1;
-    const main: Branch = new Branch('main', null);
-
-    this.branches.push(main);
-    this.head = this.branches[this.branches.length];
-  } 
-
-  commit(id: number, message: string) {
-    
-  }
 }
 
 interface Commit {
   id: number;
-  parent: Commit;
+  parentId: number;
   message: string;
 }
 
 interface Branch {
   name: string;
-  commits: string[];
+  commits: Commit[];
+}
+
+class Git {
+  private lastCommitId: number = -1;
+  head: Branch;
+  repository: string;
+  branches: Branch[] = [];
+  
+  constructor(repository: string) {
+    const main: Branch = new Branch('main', null);
+    // init branch
+    this.branches.push(main);
+    // set current head
+    this.head = main;
+    // set remote url
+    this.repository = repository;
+    
+    console.log(this.head);
+  } 
+
+  commit(message: string) {
+    const parentId: number = this.head.commits[this.head.commits.length - 1]?.id??null;
+    const commit: Commit = new Commit(++this.lastCommitId, parentId, message);
+
+    this.head.commits.push(commit);
+    console.log(`create commit: (${commit.id}) ${message}`);
+  }
+
+  checkout(branch: Branch) {
+    console.log('checkout to: ' + branch.name);
+    this.branches.push(branch);
+    
+    for(let i = 0; this.branches.length; i++) {
+      if (this.branches[i].name == branch.name) {
+        this.head = branch;
+        break;
+      }
+    }
+  }
+
+  log() {
+    console.log(this.head.commits.reverse());
+  }
+}
+
+class Commit {
+  id: number;
+  parentId: number;
+  message: string;
+
+  constructor(id: number, parentId: number, message: string) {
+    this.id = id;
+    this.parentId = parentId;
+    this.message = message;
+  }
 }
 
 class Branch {
   name: string;
-  commits: string[];
+  commits: Commit[] = [];
 
-  constructor(name: string, commit: string) {
+  constructor(name: string, commits: null|Commit[]) {
     this.name = name;
-    this.commits.push(commit);
+    this.commits = commits?.reverse()??[];
   }
 }
 
-const git = new Git("git@example.com");
+const git: Git = new Git("git@example.com");
+git.commit("My first commit!");
+git.commit("My second commit!");
+git.commit("My third commit!");
+git.log();
+
+// create and checkout from main branch
+const develop: Branch = new Branch('develop', git.head.commits);
+git.checkout(develop);
+git.commit("My commit in develop!");
+git.log();
